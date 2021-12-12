5b24aac0dadf5ea233c387492e9c52218d4a9969
Songnari <na061300@naver.com>
2021-12-12 17:36:58 +0900

    make path

--- src/libft/ft_strjoin.c
+++ src/libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: sham <sham@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 21:51:56 by nasong            #+#    #+#             */
-/*   Updated: 2021/12/12 17:03:47 by sham             ###   ########.fr       */
+/*   Updated: 2021/12/12 17:23:54 by nasong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,36 @@ char *ft_strjoin_path(const char *str1, const char *str2)
 		index++;
 	}
 	result[index] = '/';
+	index++;
+	while (*str2 != '\0')
+	{
+		result[index] = *str2;
+		str2++;
+		index++;
+	}
+	result[index] = '\0';
+	return (result);
+}
+
+char *ft_strjoin_with_char(const char *str1, const char *str2, const char c)
+{
+	int len;
+	int index;
+	char *result;
+
+	len = ft_strlen(str1) + ft_strlen(str2) + 2;
+	result = (char *)malloc(sizeof(char) * len);
+	if (result == 0)
+		return (0);
+
+	index = 0;
+	while (*str1 != '\0')
+	{
+		result[index] = *str1;
+		str1++;
+		index++;
+	}
+	result[index] = c;
 	index++;
 	while (*str2 != '\0')
 	{
