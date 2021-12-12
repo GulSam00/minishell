e3e8a641c6f2173a0e34af8e83d1788e8d519173
Sangjoon Ham <gulsamcono@gmail.com>
2021-12-12 14:49:39 +0900

    merge modify

--- src/minishell.h
+++ src/minishell.h
@@ -6,7 +6,7 @@
 /*   By: sham <sham@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 14:17:39 by sham              #+#    #+#             */
-/*   Updated: 2021/12/12 14:46:24 by sham             ###   ########.fr       */
+/*   Updated: 2021/12/12 14:48:34 by sham             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,24 +35,6 @@
 #include <fcntl.h>
 #include <termios.h> // 터미널 헤더
 
-enum e_cmd_type
-{
-	CMD = 0,
-	ARG,
-	DISCRIPTOR,
-	OPTION,
-	CMD_TYPE_MAX,
-	PIPE
-};
-
-enum e_discriptor_type
-{
-	IN,
-	OUT,
-	DOUBLE_IN,
-	DOUBLE_OUT
-};
-
 // enum e_bulit_in_code
 // {
 // 	CD = 1,
@@ -63,46 +45,6 @@ enum e_discriptor_type
 // 	PWD,
 // 	UNSET
 // };
-
-struct s_data // 디스크립터, 명령어
-{
-	void *contents;
-	struct s_data *next;
-} typedef t_data;
-
-struct s_list
-{
-	void *front;
-	int size;
-} typedef t_list;
-
-struct s_cmd
-{
-	char *cmd;	// 명령어(커맨드)
-	char **arg; // 프롬프트에 입력한 문자열?
-	struct s_list arg_list;
-} typedef t_cmd;
-
-struct s_env
-{
-	char *key;
-	char *value;
-} typedef t_env;
-
-/* list */
-void init_list(t_list *list);
-void init_cmd(t_cmd *cmd);
-int add_list(t_list *list, t_data *new_data);
-int add_data(t_list *list, void *new_contents);
-int free_cmd_list(t_list *list);
-int free_str_list(t_list *list);
-void print_cmd_list(t_list *list);
-void print_str_list(t_list *list);
-void print_env_list(t_list *list);
-void pop_env_with_key(t_list *list, char *key);
-void init_env(t_env *env, char *key, char *value);
-int free_env_list(t_list *list);
-int ft_parser(t_list *cmd_list, char *input);
 
 enum e_cmd_type
 {
