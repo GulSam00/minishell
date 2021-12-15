4cb8914a4a932bf0a6610c7042dd9fe1cab3520d
Sangjoon Ham <gulsamcono@gmail.com>
2021-12-15 17:35:54 +0900

    Feat : pwd FD 0으로 설정된 것 확인. 수정하니 정상 작동.

--- src/built_in/ft_pwd.c
+++ src/built_in/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: sham <sham@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/28 17:22:08 by sham              #+#    #+#             */
-/*   Updated: 2021/12/15 17:19:21 by sham             ###   ########.fr       */
+/*   Updated: 2021/12/15 17:32:32 by sham             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ int ft_pwd(void)
 {
     char *path;
 
-    path = getcwd(NULL, 0);
-    write(0, path, ft_strlen(path));
-    write(0, "\n", 1);   
+    path = getcwd(NULL, 1);
+    write(1, path, ft_strlen(path));
+    write(1, "\n", 1);   
 
     return (0);
 }
