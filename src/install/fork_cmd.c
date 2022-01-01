d8bcc70c09a609c063c6865ccc6725ad5adafbbc
GulSam00 <gulsamcono@gmail.com>
2022-01-01 13:01:16 +0900

    Feat : error check

--- src/install/fork_cmd.c
+++ src/install/fork_cmd.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/12 17:56:29 by sham              #+#    #+#             */
-/*   Updated: 2022/01/01 12:40:42 by marvin           ###   ########.fr       */
+/*   Updated: 2022/01/01 12:53:58 by sham             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ static void	single_cmd(t_data *data, t_list *env_list, int status)
 			parse_cmd(cmd, env_list, 0);
 		else
 		{
-			close_main_fd(cmd->discriptor)
+			close_main_fd(cmd);
 			waitpid(pid, &status, 0);
 			g_sc = WEXITSTATUS(status);
 		}
@@ -86,7 +86,7 @@ static void	multi_cmd(t_data *data, t_list *env_list, int status)
 		prev_input = fd[0];
 	}
 	close(prev_input);
-	close_main_fd(cmd->discriptor);
+	close_main_fd(cmd);
 	waitpid(pid, &status, 0);
 	g_sc = WEXITSTATUS(status);
 }
